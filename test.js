a0fd2b7feb334a7b505dd75ea4302794a24765eb
Leonardo Tallone <leonardo.gabriel.tallone@gmail.com>
2022-02-06 12:31:31 -0300

    Probando Test 01

---
+++ test.js
@@ -0,0 +1,9 @@
+const saludar = (name) => {
+  if (typeof name == "string") {
+    return `Hola, ${name}!`;
+  }
+  return "Hola!";
+};
+
+const sumar = (firtsNumber, secondNumber) => firtsNumber + secondNumber;
+
